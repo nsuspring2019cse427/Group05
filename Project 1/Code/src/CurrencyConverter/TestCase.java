9b090693916c0859541ff41ef4a20fbb14b376e4
AkifArshad <arshad.akif@northsouth.edu>
2019-04-05 17:20:42 +0600

    Updated TestCase(Input Space Partintioning added).java
    Input Space partioning added for Zero and positive 1,2,4,8,16 bit integers

--- Project 1/Code/src/CurrencyConverter/TestCase.java
+++ Project 1/Code/src/CurrencyConverter/TestCase.java
@@ -12,30 +12,194 @@ import org.junit.jupiter.api.Test;
 
 class TestCase extends Start1 {
 	@Test
-	void test() {
+	void test_For_Zero() {
 		Convert con = new Convert();
 		
 		//Valid Input
+		/**
+		 * test for input == 0
+		 */
 		
-		double result1 = con.USDtoTAKA(5);
-		assertEquals(416.5, result1);
+		double result1 = con.USDtoTAKA(0);
+		assertEquals(0, result1);
 		
-		double result2 = con.EUROtoTAKA(10);
-		assertEquals(960.6, result2);
+		double result2 = con.EUROtoTAKA(0);
+		assertEquals(0, result2);
 		
-		double result3 = con.RUPEEtoTAKA(8);
-		assertEquals(9.36, result3);
+		double result3 = con.RUPEEtoTAKA(0);
+		assertEquals(0, result3);
 		
-		double result4 = con.TAKAtoUSD(5);
-		assertEquals(0.6, result4);
+		double result4 = con.TAKAtoUSD(0);
+		assertEquals(0, result4);
 		
-		double result5 = con.TAKAtoEURO(5);
-		assertEquals(0.48030000000000006, result5);
+		double result5 = con.TAKAtoEURO(0);
+		assertEquals(0, result5);
 		
-		double result6 = con.TAKAtoRUPEE(5);
-		assertEquals(4.2, result6);
+		double result6 = con.TAKAtoRUPEE(0);
+		assertEquals(0, result6);
 		
-		//negative input
+
+		}
+	@Test
+	void test_For_Signed_1bit_Integer() {
+		Convert con = new Convert();
+		
+		//Valid Input
+		/**
+		 * test for positive input == +1
+		 */
+		
+		double result1 = con.USDtoTAKA(1);
+		assertEquals(83.3, result1);
+		
+		double result2 = con.EUROtoTAKA(1);
+		assertEquals(96.06, result2);
+		
+		double result3 = con.RUPEEtoTAKA(1);
+		assertEquals(1.17, result3);
+		
+		double result4 = con.TAKAtoUSD(1);
+		assertEquals(0.12, result4);
+		
+		double result5 = con.TAKAtoEURO(1);
+		assertEquals(0.09606, result5);
+		
+		double result6 = con.TAKAtoRUPEE(1);
+		assertEquals(0.84, result6);
+		
+
+		}
+	
+	@Test
+	void test_For_Signed_2bit_Integer() {
+		Convert con = new Convert();
+		
+		//Valid Input
+		/**
+		 * test for positive input == +3
+		
+		 */
+		
+		double result1 = con.USDtoTAKA(3);
+		assertEquals(249.89999999999998, result1);
+		
+		double result2 = con.EUROtoTAKA(3);
+		assertEquals(288.18, result2);
+		
+		double result3 = con.RUPEEtoTAKA(3);
+		assertEquals(3.51, result3);
+		
+		double result4 = con.TAKAtoUSD(3);
+		assertEquals(0.36, result4);
+		
+		double result5 = con.TAKAtoEURO(3);
+		assertEquals(0.28818, result5);
+		
+		double result6 = con.TAKAtoRUPEE(3);
+		assertEquals(2.52, result6);
+		
+
+		}
+	
+	@Test
+	void test_For_Signed_4bit_Integer() {
+		Convert con = new Convert();
+		
+		//Valid Input
+		/**
+		 * test for positive input == +6
+		 
+		 */
+		
+		double result1 = con.USDtoTAKA(6);
+		assertEquals(499.79999999999995, result1);
+		
+		double result2 = con.EUROtoTAKA(6);
+		assertEquals(576.36, result2);
+		
+		double result3 = con.RUPEEtoTAKA(6);
+		assertEquals(7.02, result3);
+		
+		double result4 = con.TAKAtoUSD(6);
+		assertEquals(.72, result4);
+		
+		double result5 = con.TAKAtoEURO(6);
+		assertEquals(.57636, result5);
+		
+		double result6 = con.TAKAtoRUPEE(6);
+		assertEquals(5.04, result6);
+		
+
+		}
+	
+	@Test
+	void test_For_Signed_8bit_Integer() {
+		Convert con = new Convert();
+		
+		//Valid Input
+		/**
+		 * test for positive input == +40
+		 
+		 */
+		
+		double result1 = con.USDtoTAKA(40);
+		assertEquals(3332, result1);
+		
+		double result2 = con.EUROtoTAKA(40);
+		assertEquals(3842.4, result2);
+		
+		double result3 = con.RUPEEtoTAKA(40);
+		assertEquals(46.8, result3);
+		
+		double result4 = con.TAKAtoUSD(40);
+		assertEquals(4.8, result4);
+		
+		double result5 = con.TAKAtoEURO(40);
+		assertEquals(3.8424000000000005, result5);
+		
+		double result6 = con.TAKAtoRUPEE(40);
+		assertEquals(33.6, result6);
+		
+
+		}
+	
+	@Test
+	void test_For_Signed_16bit_Integer() {
+		Convert con = new Convert();
+		
+		//Valid Input
+		/**
+		 * test for positive input == +40
+		 
+		 */
+		
+		double result1 = con.USDtoTAKA(2000);
+		assertEquals(166600, result1);
+		
+		double result2 = con.EUROtoTAKA(2000);
+		assertEquals(192120, result2);
+		
+		double result3 = con.RUPEEtoTAKA(2000);
+		assertEquals(2340, result3);
+		
+		double result4 = con.TAKAtoUSD(2000);
+		assertEquals(240, result4);
+		
+		double result5 = con.TAKAtoEURO(2000);
+		assertEquals(192.12, result5);
+		
+		double result6 = con.TAKAtoRUPEE(2000);
+		assertEquals(1680, result6);
+		
+
+		}
+	
+	
+	//negative input
+	
+	@Test 
+	void test_negativeInput(){
+		
 		
 		double result7 = con.USDtoTAKA(-5);
 		assertEquals(0, result7);
@@ -59,7 +223,7 @@ class TestCase extends Start1 {
 		assertEquals(0, result13);
 		
 		
-		}
+	}
 
 // test for same currency selection
 	@Test
@@ -198,21 +362,6 @@ class TestCase extends Start1 {
 		try {
 			double input = Double.parseDouble(textField_input.getText());
 			if(startBox.getSelectedItem().equals("Select") && endBox.getSelectedItem().equals("Rupee")) {
-				fail("input is invalid");
-			}
-				  
-			} 
-
-	
-		catch(Exception e) {
-			
-		}
-		
-		//for no currency selection
-		
-		try {
-			double input = Double.parseDouble(textField_input.getText());
-			if(startBox.getSelectedItem().equals("Select") && endBox.getSelectedItem().equals("Select")) {
 				fail("input is invalid");
 			}
 				  
